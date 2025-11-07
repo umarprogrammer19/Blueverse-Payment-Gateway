191ac4446d9e527c2a36040cadd4836e83e82262
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-07 16:46:56 +0500

    Done

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -41,7 +41,7 @@ export default function CheckoutForm() {
     }
 
     return (
-        <main className="max-w-7xl mx-auto px-4 py-8">
+        <main className="mx-auto px-12 py-8">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
                 {/* Left Column - Form */}
                 <div className="lg:col-span-2">
