093c17c72cdc28a3b1fa2b3ac1d626919d0cd15c
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-07 16:36:23 +0500

    Tailwind Integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className="text-3xl font-bold text-center">Blueverse Payment Gateway</h1>
