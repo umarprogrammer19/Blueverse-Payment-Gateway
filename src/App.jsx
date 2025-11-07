164e1b7c5253f20711e17780019785f172ac51d9
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-07 16:38:55 +0500

    Checkout form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import CheckoutForm from "./components/CheckoutForm"
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">Blueverse Payment Gateway</h1>
+      <CheckoutForm />
     </>
   )
 }
