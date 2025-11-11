f9b9ef3d58fc4c8a8a724fc561511820e32b9edb
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-11 14:28:52 +0500

    Membership Updation

--- src/pages/membership.jsx
+++ src/pages/membership.jsx
@@ -1,7 +1,13 @@
+import ProductSelect from "../components/sections/ProductSelect";
+import PurchaseSummary from "../components/sections/PurchaseSummary";
+
 const Membership = () => {
     return (
-        <div>
-
+        <div className="container mx-auto py-14 max-w-sm">
+            <ProductSelect />
+            <div className="mt-10">
+                <PurchaseSummary />
+            </div>
         </div>
     )
 }
