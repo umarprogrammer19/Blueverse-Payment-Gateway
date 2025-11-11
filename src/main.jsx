933381802eb0ce076361456f1e80475d760ab6df
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-11 11:56:23 +0500

    Routing Configured Correctly

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,28 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <App />,
+      },
+      {
+        path: "/membership",
+        element: <Membership />,
+      },
+      {
+        path: "/payment",
+        element: <Payment />,
+      },
+    ],
   },
-  {
-    path: "/membership",
-    element: <Membership />,
-  },
-  {
-    path: "/payment",
-    element: <Payment />,
-  },
+
 ]);
 
 createRoot(document.getElementById('root')).render(
