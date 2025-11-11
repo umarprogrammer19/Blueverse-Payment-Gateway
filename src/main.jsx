fb59908476c7d13097d7c2d59c5eb1ab35207d7b
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-11 16:43:49 +0500

    Checkout Context Added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './layout.jsx';
 import Membership from './pages/membership.jsx';
 import Payment from './pages/payment.jsx';
+import { CheckoutProvider } from './context/CheckoutContext.jsx';
 
 const router = createBrowserRouter([
   {
@@ -29,5 +30,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <CheckoutProvider>
+    <RouterProvider router={router} />
+  </CheckoutProvider>
 )
