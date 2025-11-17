e21e5ac7d816f17325df4147f7c95a4954015e0d
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-17 17:55:52 +0500

    Added in layout

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,8 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from './components/header';
+
 const Layout = () => {
     return (
-        <Outlet />
+        <>
+            <Header />
+            <Outlet />
+        </>
     )
 }
 
