2c0f8eced1e722c42049a235fb23e17a939b2905
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-25 14:42:53 +0500

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <header className="w-full h-auto lg:h-[90px] bg-white border-b border-gray-400">
             <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
                 {/* Logo */}
-                <a href="/" className="flex items-center gap-2">
+                <a href="https://wheat-ferret-827560.hostingersite.com/" className="flex items-center gap-2">
                     <img src="/logo.png" alt="BlueVerse" className="h-9 md:h-14 w-auto" />
                 </a>
 
