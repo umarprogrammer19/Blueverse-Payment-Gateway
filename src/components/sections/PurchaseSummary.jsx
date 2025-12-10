dd8f50a4405153619b4689ad8808de597b78ef46
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-12-10 17:21:20 +0500

    Success

--- src/components/sections/PurchaseSummary.jsx
+++ src/components/sections/PurchaseSummary.jsx
@@ -94,7 +94,7 @@ export default function PurchaseSummary({
             timezone,
             txndatetime,
             storename: storeName,
-            chargetotal: chargeTotal,
+            chargetotal: 2,
             currency,
             paymentMethod,
             oid,
