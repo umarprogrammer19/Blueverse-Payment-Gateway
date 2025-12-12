666e5095de0481d7dee3dcf1c4b099446500df3f
developers-xntric <developers@xntric.me>
2025-12-12 14:38:37 +0500

    test

--- src/components/sections/PurchaseSummary.jsx
+++ src/components/sections/PurchaseSummary.jsx
@@ -97,7 +97,7 @@ export default function PurchaseSummary({
             timezone,
             txndatetime,
             storename: storeName,
-            chargetotal: 400,
+            chargetotal: chargeTotal,
             currency,
             paymentMethod,
             oid,
