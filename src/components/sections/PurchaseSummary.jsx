f70bd93bf35285bfcfb87e86600ad612449097ec
developers-xntric <developers@xntric.me>
2025-12-12 14:41:45 +0500

    reverted

--- src/components/sections/PurchaseSummary.jsx
+++ src/components/sections/PurchaseSummary.jsx
@@ -97,7 +97,7 @@ export default function PurchaseSummary({
             timezone,
             txndatetime,
             storename: storeName,
-            chargetotal: chargeTotal,
+            chargetotal: 2,
             currency,
             paymentMethod,
             oid,
